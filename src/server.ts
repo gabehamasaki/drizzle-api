6fea82bdfacb30850d7232a9cb473c7cf7af0de9
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-02-23 16:34:16 -0300

    Adicionando error handling

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import Fastify from 'fastify'
 import fs from 'fs'
 import env from '@/utils/env';
 import logger from '@/utils/log';
+import errorHandler from './utils/error';
 
+// Application logger
 const app = Fastify({
   logger
 });
 
+// Application error handler
+app.setErrorHandler((error, request, reply) => errorHandler(app, error, request, reply));
 
 // Dynamic import routes
 fs.readdirSync('./src/routes/').forEach(file => {
