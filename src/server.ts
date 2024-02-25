26d8161a630c6518f0c86423b61104b8c2ef1017
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2024-02-25 09:53:26 -0300

    Adicionando integração com CloudFlare R2

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import fs from 'fs'
 import env from '@/utils/env';
 import logger from '@/utils/log';
 import errorHandler from './utils/error';
+import migrate from './database/migrate';
 
 // Application logger
 const app = Fastify({
@@ -11,6 +12,10 @@ const app = Fastify({
 
 // Application error handler
 app.setErrorHandler((error, request, reply) => errorHandler(app, error, request, reply));
+
+app.addHook('onReady', async () => {
+  await migrate(app);
+});
 
 // Dynamic import routes
 fs.readdirSync('./src/routes/').forEach(file => {
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,11 +2,16 @@ import { z } from "zod";
 
 const envSchema = z.object({
   PORT: z.string().default('3333').transform((val) => Number(val)),
+  // Databases
   DATABASE_HOST: z.string(),
   DATABASE_PORT: z.string().transform((val) => Number(val)),
   DATABASE_NAME: z.string(),
   DATABASE_USER: z.string(),
   DATABASE_PASSWORD: z.string(),
+  // Cloudflare R2
+  CLOUDFLARE_ENDPOINT: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
 })
 
 const env = envSchema.parse(process.env);
